a040bf3aa74ecb3811668f81158ce50c774f261b
Sergey Gorokhov <sergey.login@gmail.com>
2019-10-03 23:10:51 +0300

    Seed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_182809) do
+ActiveRecord::Schema.define(version: 2019_10_03_194049) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
