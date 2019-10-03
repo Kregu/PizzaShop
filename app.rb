7a31158235d1095bd6f94a4ecbb1a1c236c84e0e
Sergey Gorokhov <sergey.login@gmail.com>
2019-10-03 21:39:24 +0300

    Add Product model and create migration

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+end
+
 
 configure do
   enable :sessions
@@ -26,23 +33,4 @@ end
 
 get '/about' do
   erb :about
-end
-
-get '/login/form' do
-  erb :login_form
-end
-
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
-end
-
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
 end
