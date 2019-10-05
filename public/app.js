5a6b024a139282a4558ae4854c3ffb8925a14df9
Sergey Gorokhov <sergey.login@gmail.com>
2019-10-05 19:10:04 +0300

    Calculate products in cart

--- public/app.js
+++ public/app.js
@@ -3,8 +3,24 @@ function something() {
 }
 
 function add_to_cart(id) {
-	var key = "product" + id
-	var x = localStorage.getItem (key)
+	var key = "product_" + id
+	var x = window.localStorage.getItem(key)
 	x = x * 1 + 1
-	localStorage.setItem (key, x)
+	window.localStorage.setItem(key, x)
+
+	alert(('Item in your cart: ') + cart_get_number_of_item());
+}
+
+function cart_get_number_of_item() {
+	var cnt = 0
+	for (var i = 0; i < window.localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+
+    if (key.indexOf('product_') == 0)
+    {
+    	cnt = cnt + value * 1;
+    }
+}
+	return cnt;
 }
