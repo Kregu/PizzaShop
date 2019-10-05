017f93095c6935ea737c00d0f724e209479807cf
Sergey Gorokhov <sergey.login@gmail.com>
2019-10-05 16:34:28 +0300

    Save products to localStorage

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
 function something() {
 	alert("Hello!");
 }
+
+function add_to_cart(id) {
+	var key = "product" + id
+	var x = localStorage.getItem (key)
+	x = x * 1 + 1
+	localStorage.setItem (key, x)
+}
