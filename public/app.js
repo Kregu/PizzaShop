657787abb53d2bf8f4709ab42c0ea0bfd48d2d08
Sergey Gorokhov <sergey.login@gmail.com>
2021-10-11 23:45:49 +0300

    Code refactoring

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 function add_to_cart(id) {
-	var key = "product_" + id;
-	var x = window.localStorage.getItem(key);
+	let key = "product_" + id;
+	let x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
@@ -10,41 +10,41 @@ function add_to_cart(id) {
 }
 
 function update_orders_input()	{
-	var orders = cart_get_orders();
+	let orders = cart_get_orders();
 	$('#orders_input').val(orders);
 
 	}
 function update_orders_button()	{
-	var text = 'Cart (' + cart_get_number_of_item() + ')';
+	let text = 'Cart (' + cart_get_number_of_item() + ')';
 	$('#orders_button').val(text);
 
 	}
 
 function cart_get_number_of_item() {
-	var cnt = 0
-	for (var i = 0; i < window.localStorage.length; i++) {
-    var key = localStorage.key(i);
-    var value = window.localStorage.getItem(key);
+	let cnt = 0;
+	for (let i = 0; i < window.localStorage.length; i++) {
+		let key = localStorage.key(i);
+		let value = window.localStorage.getItem(key);
 
-    if (key.indexOf('product_') == 0)
-    {
-    	cnt = cnt + value * 1;
-    }
-}
+		if (key.indexOf('product_') != 0) {
+			continue;
+		}
+		cnt = cnt + value * 1;
+	}
 	return cnt;
 }
 
 function cart_get_orders() {
-	var orders = ''
-	for (var i = 0; i < window.localStorage.length; i++) {
-    var key = localStorage.key(i);
-    var value = window.localStorage.getItem(key);
+	let orders = '';
+	for (let i = 0; i < window.localStorage.length; i++) {
+		let key = localStorage.key(i);
+		let value = window.localStorage.getItem(key);
 
-    if (key.indexOf('product_') == 0)
-    {
-    	orders = orders + key + '=' + value + ',';
-    }
-}
+		if (key.indexOf('product_') != 0) {
+			continue;
+		}
+		orders = orders + key + '=' + value + ',';
+	}
 	return orders;
 }
 
